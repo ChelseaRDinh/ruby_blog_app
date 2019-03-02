eb41b75250bc9182908d307f4d652062a4df9f28
chelseardinh <chelseadinh94@gmail.com>
2019-03-01 17:16:57 -0800

    Added functionality to delete comments. Added Delete Comment button in comments view.

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
 	#Add restriction for length of blog title and content input.
+	has_many :comments
 	validates :title, presence: true,
 					  length: { minimum: 5 }
 end
